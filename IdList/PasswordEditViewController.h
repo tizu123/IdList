dbd1b4dcbf833443a6bf7c062d80c299157effda
t izu <t.izu.123@gmail.com>
2012-08-03 13:28:31 +0900

    RootPasswordの編集画面とそこへの遷移を追加

---
+++ IdList/PasswordEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  PasswordEditViewController.h
+//  IdList
+//
+//  Created by tizu123 on 12/08/03.
+//  Copyright (c) 2012年 パパ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PasswordEditViewController : UIViewController
+
+@end
---
+++ IdList/PasswordEditViewController.m
@@ -0,0 +1,43 @@
+//
+//  PasswordEditViewController.m
+//  IdList
+//
+//  Created by tizu123 on 12/08/03.
+//  Copyright (c) 2012年 パパ. All rights reserved.
+//
+
+#import "PasswordEditViewController.h"
+
+@interface PasswordEditViewController ()
+
+@end
+
+@implementation PasswordEditViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
