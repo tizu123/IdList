c5ba7610038447393a0dcc862c8e8683afa411fc
t izu <t.izu.123@gmail.com>
2012-08-08 20:50:55 +0900

    パスワードをkeychainに保存するように変更

--- IdList/Account.h
+++ IdList/Account.h
@@ -2,19 +2,24 @@
 //  Account.h
 //  IdList
 //
-//  Created by パパ on 2012/08/01.
+//  Created by tizu123 on 12/08/08.
 //  Copyright (c) 2012年 パパ. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
+#import "KeychainItemWrapper.h"
+
 
 @interface Account : NSManagedObject
 
-@property (nonatomic, retain) NSString *title;
-@property (nonatomic, retain) NSString *loginId;
-@property (nonatomic, retain) NSString *subId;
-@property (nonatomic, retain) NSString *password;
-@property (nonatomic, retain) NSString *url;
-@property (nonatomic, retain) NSString *memo;
+@property (nonatomic, retain) NSString * loginId;
+@property (nonatomic, retain) NSString * memo;
+@property (nonatomic, retain) NSString * subId;
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSString * url;
+@property (nonatomic, retain) NSString * password;
+
+@property (nonatomic, retain) KeychainItemWrapper *keychainItem;
 
 @end
--- IdList/Account.m
+++ IdList/Account.m
@@ -2,19 +2,37 @@
 //  Account.m
 //  IdList
 //
-//  Created by パパ on 2012/08/01.
+//  Created by tizu123 on 12/08/08.
 //  Copyright (c) 2012年 パパ. All rights reserved.
 //
 
 #import "Account.h"
+#import "Security/Security.h"
 
 @implementation Account
 
-@dynamic title;
 @dynamic loginId;
-@dynamic subId;
-@dynamic password;
-@dynamic url;
 @dynamic memo;
+@dynamic subId;
+@dynamic title;
+@dynamic url;
 
+- (NSString *)password
+{
+    if (!self.keychainItem) {
+        self.keychainItem = [[KeychainItemWrapper alloc] initWithIdentifier:self.objectID.description accessGroup:nil];
+    }
+    return [self.keychainItem objectForKey:(__bridge id)(kSecAttrService)];
+
+}
+
+- (void)setPassword:(NSString *)password
+{
+    if (!self.keychainItem) {
+        self.keychainItem = [[KeychainItemWrapper alloc] initWithIdentifier:self.objectID.description accessGroup:nil];
+    }
+    [self.keychainItem setObject:password forKey:(__bridge id)(kSecAttrService)];
+}
+
+ 
 @end
